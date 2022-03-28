725a567a0979ac70a07b2b1b2803b443c5897a5c
Abhishekkumar11111 <abhishekkumar857885@gmail.com>
2022-03-28 21:27:08 +0530

    vehiclebackened

---
+++ vehiclebackend/src/main/java/com/lti/appl/VehiclebackendApplication.java
@@ -0,0 +1,13 @@
+package com.lti.appl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclebackendApplication.class, args);
+	}
+
+}
